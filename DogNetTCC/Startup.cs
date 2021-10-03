9e6c7bc7c1e51a1eb876ec4eb8a609b8ef678e7e
Gabriel Marchi <86965269+gabrielhcmarchi@users.noreply.github.com>
2021-10-03 18:38:48 -0300

    versão 2

---
+++ DogNetTCC/Startup.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using DogNet.Data;
+using DogNet.Services;
+using System.Globalization;
+using DogNet.Entities;
+using DogNet.Models;
+
+namespace DogNet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // habilita a necessidade de consentimento para uso de cookie
+                options.CheckConsentNeeded = context => true;
+                // adicionar using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true; //default = false
+                options.Password.RequireNonAlphanumeric = false; //default = true
+                options.Password.RequireUppercase = false; //default = true
+                options.Password.RequireLowercase = false; //default = true               
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3); //default = 3
+                options.Lockout.MaxFailedAccessAttempts = 3; //default = 5
+                options.SignIn.RequireConfirmedAccount = false; //default = false
+                options.SignIn.RequireConfirmedEmail = true; //default = false
+                options.SignIn.RequireConfirmedPhoneNumber = false; //default = false                
+            }).AddEntityFrameworkStores<DogNetMvcContext>()
+              .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.LoginPath = "/Login";
+                options.AccessDeniedPath = "/Login";
+                options.SlidingExpiration = true;
+            });
+
+            services.AddAuthorization(options =>
+            {
+                //adiciona uma política de acesso chamada isAdmin
+                options.AddPolicy("isAdmin", policy =>
+                    policy.RequireRole("admin"));
+            });
+
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AuthorizePage("/Admin/Admin", "isAdmin");
+                options.Conventions.AuthorizeFolder("/Admin/ProdutoCRUD", "isAdmin");
+            }).AddCookieTempDataProvider(options =>
+            {
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddMvc();
+
+            string mySqlConnectionStr = Configuration.GetConnectionString("DogNetMvcContext");
+
+          
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<DogNetMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("DogNetMvcContext"), builder =>
+                        builder.MigrationsAssembly("DogNet")));
+
+
+
+            services.Configure<EmailConfiguration>(Configuration.GetSection("EmailConfiguration"));
+            //services.AddSingleton<IEmailSender, EmailSender>();
+            services.AddSingleton<IEmailSender, SendGridSender>();
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            
+            services.AddControllers();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+     
+
+           
+
+           
+           
+
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+
+            var defaultCulture = new CultureInfo("pt-BR");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+     
+        }
+    }
+}
---
+++ DogNetTCC/insert_produtos.sql
@@ -0,0 +1,43 @@
+﻿INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+1,'Coleira de Identificação','Coleira de identificação pra segurança',25.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+2,'Peitoral pra Passeio','Passeie com qualidade e segurança com seu melhor amigo.',65.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+3,'Guias pra passeio','Guias personalizadas para passeio.',80.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+4,'Cintos de segurança','Cintos de segurança personalizados.',130.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+5,'Guias duplas','Guia dupla, pra um passeio em dobro.',110.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+6,'Snacks','Petiscos low fat pro seu amiguinho.',40.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+7,'Ração Golden Special Sabor Frango e Carne para Cães Adultos','Ração de alta qualidade com pedigree.',225.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+8,'Golden Power Training Filhote para Cães Sabor Frango e Arroz - 15kg','Ração de alta qualidade com pedigree.',119.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+9,'Biscoito Golden Cookie para Cães Adultos 350g','Ração de alta qualidade com pedigree',55.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+10,'Ração Golden Fórmula Mini Bits para Cães Filhotes de Pequeno Porte Sabor Frango e Arroz','Ração de alta qualidade com pedigree',225.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+11,'Recovery Royal Canin Veterinary Ração Lata Cães e Gatos 195 g','Ração de alta qualidade com pedigree',25.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+12,'Poste Arranhador DogNet para Gatos - Cores Sortidas','Ao invés de comprar outro sofá, compre isso.',110.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+13,'Brinquedo Arranhador DogNet House Marrom para Gatos','Não aguenta mais ter que comprar cortinas novas? Essa é a solução.',140.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+14,'Spray Cat Nip Cat Dog para Gatos 100ml','Pro seu gato ficar mais doidão do que ele ja é.',15.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+15,'Ração Golden para Gatos Adultos Castrados Sabor Salmão','Ração de alta qualidade com pedigree',145.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+16,'Ração Golden para Gatos Adultos Castrados Seleção Natural Abóbora','Ração de alta qualidade com pedigree',145.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+17,'Ração Úmida Royal Canin Sachê Instinctive - Gatos Adultos - 85g','Ração de alta qualidade com pedigree',35.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+18,'Ração Royal Canin Premium Cat para Gatos Adultos Castrados','Ração de alta qualidade com pedigree',25.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+19,'Ração Royal Canin Veterinary Obesity - Gatos Adultos - 1,5kg','Ração de alta qualidade com pedigree',155.69,20);
+INSERT INTO dognetdb.produtos (IdProduto,Nome,Descricao,Preco,Estoque) VALUES (
+20,'Ração Royal Canin Exigent - Gatos Adultos','Ração de alta qualidade com pedigree',125.69,20);
+
+
+
