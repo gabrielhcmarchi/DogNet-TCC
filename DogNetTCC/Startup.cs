d38e85d4eb80ab8b0e446e4e24893b861183004e
Gabriel Coelho <coelhogabriel2411@gmail.com>
2021-10-07 08:49:17 -0300

    adm está onn

--- DogNetTCC/Startup.cs
+++ DogNetTCC/Startup.cs
@@ -47,7 +47,7 @@ namespace DogNet
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3); //default = 3
                 options.Lockout.MaxFailedAccessAttempts = 3; //default = 5
                 options.SignIn.RequireConfirmedAccount = false; //default = false
-                options.SignIn.RequireConfirmedEmail = true; //default = false
+                options.SignIn.RequireConfirmedEmail = false; //default = false
                 options.SignIn.RequireConfirmedPhoneNumber = false; //default = false                
             }).AddEntityFrameworkStores<DogNetMvcContext>()
               .AddDefaultTokenProviders();
