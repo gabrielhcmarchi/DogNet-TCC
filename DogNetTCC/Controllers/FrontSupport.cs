32184107a14187495bf74579d2e17e38ebf36ad9
João Victor Furquim <71611166+victorfurquim@users.noreply.github.com>
2021-10-14 11:45:01 -0300

    - SqlLite connection

--- DogNetTCC/Controllers/FrontSupport.cs
+++ DogNetTCC/Controllers/FrontSupport.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DogNet.Models;
+using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +10,31 @@ namespace DogNet.Controllers
 {
     public static class FrontSupport
     {
+
        public static List<Models.Instituicoes> ReturnInstituicoes()
         {
-            return new List<Models.Instituicoes>();
+
+            using var con = new SqliteConnection("Filename=./dados.db");
+            con.Open();
+
+            string stm = "SELECT Nome FROM Instituicoes";
+
+            using var cmd = new SqliteCommand(stm, con);
+            using SqliteDataReader rdr = cmd.ExecuteReader();
+
+            Instituicoes vwInst = new Instituicoes();
+            List<Instituicoes> inst = new List<Instituicoes>();
+
+            while (rdr.Read())
+            {      
+                vwInst.Nome =  rdr.GetString(1);
+                inst.Add(vwInst);
+            }
+
+            con.Close();
+            return inst;
+
+
         }
     }
 }
